9c2260ed5e342b3fc17cc9b4ad923a2d4a7a56df
Jinyu (Jaden) Wang <jinyuwang0707@gmail.com>
2015-12-29 17:04:31 -0800

    task & job code

---
+++ CodeSnippet/Task1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSnippet
+{
+    class Task1
+    {
+        public static void Main()
+        {
+            Task task1 = new Task(() => { Console.WriteLine("This is from task #1."); });
+            Task task2 = new Task(() => PrintMessage(2));
+
+            task1.Start();
+            task2.Start();
+            Console.ReadLine();
+        }
+
+        private static void PrintMessage(int task)
+        {
+            Console.WriteLine(string.Format("This is from task #{0}", task));
+        }
+    }
+}
---
+++ CodeSnippet/Task2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSnippet
+{
+    class Task2
+    {
+        public static void Main()
+        {
+            Task<int> task1 = new Task<int>(() => { return Sum(100); });
+            task1.Start();
+            //task1.Wait();
+            Console.WriteLine(task1.Result); // Result will automatically call wait
+
+            var task2 = Task.Factory.StartNew(() => Console.WriteLine("create and start new task by startnew;"));
+            var task3 = 
+                Task.Run(
+                     () => Console.WriteLine("create and start new task by run")    
+                    );
+            Task.WaitAll(task1, task2, task3);
+        }
+
+        private static int Sum(int n)
+        {
+            int sum = 0;
+            for (int i = 0; i <= n; i++ )
+            {
+                sum += i;
+            }
+            return sum;
+        }
+    }
+}
